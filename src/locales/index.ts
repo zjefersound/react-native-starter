faf646401210f92eed18ed99d169dfad82464069
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2022-08-25 15:47:34 -0300

    change language feature

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,7 +1,9 @@
 import en from '@locales/en.json';
+import pt from '@locales/pt.json';
 
 export const defaultLng = 'en';
 
 export const resources = {
   en,
+  pt
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {useTranslation} from 'react-i18next';
 
 const HomeScreen = () => {
-  const { t } = useTranslation();
+  const [t, i18n] = useTranslation();
+  const {changeLanguage} = i18n;
   return (
-    <View className='bg-slate-900 flex-1'>
-      <Text className='text-white text-2xl'>{t("Welcome to React")}</Text>
+    <View className="bg-slate-900 flex-1 p-4">
+      <Text className="text-white text-3xl font-bold">{t('Welcome to React')}</Text>
+      <TouchableOpacity
+        className="bg-green-700 rounded p-3 mt-4"
+        onPress={() => changeLanguage('pt')}>
+        <Text className="text-white text-lg">{t('Portuguese')}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        className="bg-blue-700 rounded p-3 mt-4"
+        onPress={() => changeLanguage('en')}>
+        <Text className="text-white text-lg">{t('English')}</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
